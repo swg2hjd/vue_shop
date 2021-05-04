efe98e10b51f569a70dffa38edf6e8be2828ab6a
swg_glb <137707189_glb@qq.com>
2021-05-04 22:20:07 +0800

    '完成了数据报表的开发'

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ const Cate = () => import('../views/goods/Cate')
 const Params = () => import('../views/goods/Params')
 const List = () => import('../views/goods/List')
 const Order = ()=>import('../views/order/Order')
+const Report = ()=>import('../views/report/Report')
 
 Vue.use(VueRouter)
 
@@ -70,6 +71,11 @@ const routes = [
         path: '/orders',
         name: 'Order',
         component: Order
+      },
+      {
+        path: '/reports',
+        name: 'Report',
+        component: Report
       }
     ]
   }
